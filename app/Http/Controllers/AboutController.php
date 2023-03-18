7abcd7c94002e3e2e9b452862b8168c5b136d142
evanfadilahd <yiersanshefan@gmail.com>
2023-03-18 20:57:36 +0700

    02_praktikum_web_lanjut_dua

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+
+    public function index()
+    {
+        echo"<br> Nama : Evan Fadhilah Dzulfikar";
+        echo"<br> NIM : 2141720167";
+    }
+
+    public function create()
+    {
+        //
+    }
+
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/practicum02', [PageController::class, 'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+Route::get('/home', [HomeController::class, 'index']);
+Route::get('/abouts', [AboutController::class, 'index']);
+Route::get('/articles/{id}', [ArticlesController::class, 'index']);
